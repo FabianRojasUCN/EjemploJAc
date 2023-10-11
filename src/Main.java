bf8c3af2feedab960f8cf50864e844b837ccc3e1
Fabian Rojas <fabian.rojas.arce95@gmail.com>
2023-10-11 11:12:37 -0300

    Se agrega función de hilos

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
-        System.out.println("Hola Mundo!");
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        for (int i = 1; i <= 10; i++) {
+            int number = i;
+            executor.execute(() -> {
+                System.out.println(number);
+            });
+        }
+
+        executor.shutdown();
+        executor.awaitTermination(1, TimeUnit.DAYS);
+        System.out.println("Tareas Terminadas");
     }
 }
